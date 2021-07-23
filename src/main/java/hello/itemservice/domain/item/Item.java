bcef9c32eff3befdd0d8641fbc2242309a867f43
yoonmin-kim <rladbsals23@gmail.com>
2021-07-23 21:48:39 +0900

    feat : BeanValidation - groups
    SaveCheck, UpdateCheck 인터페이스를 생성하여
    각 필드 검증어노테이션에 groups옵션으로 지정한다.
    
    컨트롤러 @Validated value로 인터페이를 넘겨주면
    groups 기능이 동작한다.
    
    gourps기능은 코드의 복잡도가 증가하여 잘 사용하지 않는다.
    등록용ModelAttribute와 수정용 ModelAttribute를 별도로 사용하는
    것이 실무에서 더 유용하다.

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량은 10000이상입니다.")
 public class Item {
 
-    @NotNull //수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    // @Max(9999) //수정 요구사항 추가
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
